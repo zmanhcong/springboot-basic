2173393157288cc37c6f60339c8b71c47199d3d8
nguyenmanhcong <cng.buff@gmail.com>
2022-11-16 01:05:09 +0900

    Category controller and html for category

--- src/main/java/com/example/springboot_basic/controller/admin/CategoryController.java
+++ src/main/java/com/example/springboot_basic/controller/admin/CategoryController.java
@@ -1,4 +1,18 @@
 package com.example.springboot_basic.controller.admin;
 
+import com.example.springboot_basic.model.CategoryDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin/categories")
 public class CategoryController{
+    @GetMapping("add")
+    public String add(Model model){
+        model.addAttribute("category", new CategoryDto());
+        return "admin/categories/addOrEdit";
+    }
+
 }
--- src/main/java/com/example/springboot_basic/model/CategoryDto.java
+++ src/main/java/com/example/springboot_basic/model/CategoryDto.java
@@ -1,13 +1,20 @@
 package com.example.springboot_basic.model;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CategoryDto {
+public class CategoryDto implements Serializable{  /*add Serializable for check input of user*/
     private int categoryId;
+    @NotEmpty
+    @Length(min = 5)
     private String name;
 }
