be8fedd9b3311cc403817eb4dae55ff91f916853
nguyenmanhcong <cng.buff@gmail.com>
2022-11-27 20:42:46 +0900

    Category: Service and Serviceimple refacor. ( add list<Category> FindAll, because cannot .stream().map -> that why need refacter

--- src/main/java/com/example/springboot_basic/service/CategoryService.java
+++ src/main/java/com/example/springboot_basic/service/CategoryService.java
@@ -1,24 +1,19 @@
 package com.example.springboot_basic.service;
 
 import com.example.springboot_basic.domain.Category;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.FluentQuery;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 public interface CategoryService extends CrudRepository<Category, Long> {
-    Page<Category> findAll(Pageable pageable);
-
-    <S extends Category> S save(S entity);
-
-    <S extends Category> Iterable<S> saveAll(Iterable<S> entities);
-
-    Optional<Category> findById(Long aLong);
-
-    boolean existsById(Long aLong);
 
 
     Page<Category> findByNameContaining(String name, Pageable pageable);
@@ -28,9 +23,52 @@ public interface CategoryService extends CrudRepository<Category, Long> {
 
     List<Category> findByNameContaining(String name);
 
-    Iterable<Category> findAll();
 
-    Iterable<Category> findAllById(Iterable<Long> longs);
+    List<Category> findAll(Sort sort);
+
+    List<Category> findAllById(Iterable<Long> longs);
+
+    <S extends Category> List<S> saveAll(Iterable<S> entities);
+
+    void flush();
+
+    <S extends Category> S saveAndFlush(S entity);
+
+    <S extends Category> List<S> saveAllAndFlush(Iterable<S> entities);
+
+    @Deprecated
+    void deleteInBatch(Iterable<Category> entities);
+
+    void deleteAllInBatch(Iterable<Category> entities);
+
+    void deleteAllByIdInBatch(Iterable<Long> longs);
+
+    void deleteAllInBatch();
+
+    @Deprecated
+    Category getOne(Long aLong);
+
+    @Deprecated
+    Category getById(Long aLong);
+
+    Category getReferenceById(Long aLong);
+
+    <S extends Category> List<S> findAll(Example<S> example);
+
+    <S extends Category> List<S> findAll(Example<S> example, Sort sort);
+
+    Page<Category> findAll(Pageable pageable);
+
+    <S extends Category> S save(S entity);
+
+    Optional<Category> findById(Long aLong);
+
+    boolean existsById(Long aLong);
+
+    @Override
+    default List<Category> findAll() {
+        return null;
+    }
 
     long count();
 
@@ -40,5 +78,17 @@ public interface CategoryService extends CrudRepository<Category, Long> {
 
     void deleteAllById(Iterable<? extends Long> longs);
 
+    void deleteAll(Iterable<? extends Category> entities);
 
+    void deleteAll();
+
+    <S extends Category> Optional<S> findOne(Example<S> example);
+
+    <S extends Category> Page<S> findAll(Example<S> example, Pageable pageable);
+
+    <S extends Category> long count(Example<S> example);
+
+    <S extends Category> boolean exists(Example<S> example);
+
+    <S extends Category, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction);
 }
--- src/main/java/com/example/springboot_basic/service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/example/springboot_basic/service/Impl/CategoryServiceImpl.java
@@ -38,69 +38,80 @@ public class CategoryServiceImpl implements CategoryService {
         return categoryRepository.findByNameContaining(name);
     }
 
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
-    }
-
+    @Override
     public List<Category> findAll(Sort sort) {
         return categoryRepository.findAll(sort);
     }
 
+    @Override
     public List<Category> findAllById(Iterable<Long> longs) {
         return categoryRepository.findAllById(longs);
     }
 
+    @Override
     public <S extends Category> List<S> saveAll(Iterable<S> entities) {
         return categoryRepository.saveAll(entities);
     }
 
+    @Override
     public void flush() {
         categoryRepository.flush();
     }
 
+    @Override
     public <S extends Category> S saveAndFlush(S entity) {
         return categoryRepository.saveAndFlush(entity);
     }
 
+    @Override
     public <S extends Category> List<S> saveAllAndFlush(Iterable<S> entities) {
         return categoryRepository.saveAllAndFlush(entities);
     }
 
+    @Override
     @Deprecated
     public void deleteInBatch(Iterable<Category> entities) {
         categoryRepository.deleteInBatch(entities);
     }
 
+    @Override
     public void deleteAllInBatch(Iterable<Category> entities) {
         categoryRepository.deleteAllInBatch(entities);
     }
 
+    @Override
     public void deleteAllByIdInBatch(Iterable<Long> longs) {
         categoryRepository.deleteAllByIdInBatch(longs);
     }
 
+    @Override
     public void deleteAllInBatch() {
         categoryRepository.deleteAllInBatch();
     }
 
+    @Override
     @Deprecated
     public Category getOne(Long aLong) {
         return categoryRepository.getOne(aLong);
     }
 
+    @Override
     @Deprecated
     public Category getById(Long aLong) {
         return categoryRepository.getById(aLong);
     }
 
+    @Override
     public Category getReferenceById(Long aLong) {
         return categoryRepository.getReferenceById(aLong);
     }
 
+    @Override
     public <S extends Category> List<S> findAll(Example<S> example) {
         return categoryRepository.findAll(example);
     }
 
+    @Override
     public <S extends Category> List<S> findAll(Example<S> example, Sort sort) {
         return categoryRepository.findAll(example, sort);
     }
@@ -110,58 +121,72 @@ public class CategoryServiceImpl implements CategoryService {
         return categoryRepository.findAll(pageable);
     }
 
+    @Override
     public <S extends Category> S save(S entity) {
         return categoryRepository.save(entity);
     }
 
+    @Override
     public Optional<Category> findById(Long aLong) {
         return categoryRepository.findById(aLong);
     }
 
+    @Override
     public boolean existsById(Long aLong) {
         return categoryRepository.existsById(aLong);
     }
 
+    @Override
     public long count() {
         return categoryRepository.count();
     }
 
+    @Override
     public void deleteById(Long aLong) {
         categoryRepository.deleteById(aLong);
     }
 
+    @Override
     public void delete(Category entity) {
         categoryRepository.delete(entity);
     }
 
+    @Override
     public void deleteAllById(Iterable<? extends Long> longs) {
         categoryRepository.deleteAllById(longs);
     }
 
+    @Override
     public void deleteAll(Iterable<? extends Category> entities) {
         categoryRepository.deleteAll(entities);
     }
 
+    @Override
     public void deleteAll() {
         categoryRepository.deleteAll();
     }
 
+    @Override
     public <S extends Category> Optional<S> findOne(Example<S> example) {
         return categoryRepository.findOne(example);
     }
 
+    @Override
     public <S extends Category> Page<S> findAll(Example<S> example, Pageable pageable) {
         return categoryRepository.findAll(example, pageable);
     }
 
+    @Override
     public <S extends Category> long count(Example<S> example) {
         return categoryRepository.count(example);
     }
 
+    @Override
     public <S extends Category> boolean exists(Example<S> example) {
         return categoryRepository.exists(example);
     }
 
+    @Override
     public <S extends Category, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
         return categoryRepository.findBy(example, queryFunction);
     }
